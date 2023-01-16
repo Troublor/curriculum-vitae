8905ae75ba7afb451e505ae8d3a2f7259505038d
William Aaron Cheung <troublor@live.com>
2023-01-16 22:05:11 +0800

    Test CI (#1)
    * update
    
    * update script
    
    * update scripts
    
    * update scripts
    
    * update script
    
    * update scripts
    
    * update scripts
    
    * update

---
+++ scripts/format-tex.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+# this scripts formats tex files
+#
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT="$DIR/.."
+
+OUT_DIR="$ROOT/.latexindent"
+mkdir -p "$OUT_DIR"
+
+if [ -z "$LATEXINDENT" ]; then
+    LATEXINDENT=latexindent
+fi
+
+recurse() {
+ for i in "$1"/*;do
+    if [ -d "$i" ];then
+        recurse "$i"
+    elif [ -f "$i" ]; then
+        if [[ "$i" == *.tex ]]; then
+            echo "Formatting $i"
+            "$LATEXINDENT" -s -l "$ROOT/.latexindent.yaml" -c "$OUT_DIR" -w $file
+        fi
+    fi
+ done
+}
+recurse
