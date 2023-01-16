8706f76b3eebe5f2d64322ce088d95e57f43813f
William Aaron Cheung <troublor@live.com>
2023-01-16 21:33:00 +0800

    update scripts

--- scripts/format-tex.sh
+++ scripts/format-tex.sh
@@ -8,8 +8,12 @@ ROOT="$DIR/.."
 OUT_DIR="$ROOT/.latexindent"
 mkdir -p "$OUT_DIR"
 
+if [ -z "$LATEXINDENT" ]; then
+    LATEXINDENT=latexindent
+fi
+
 setopt extendedglob
 for file in "$ROOT"/**/*.tex~"$ROOT"/template/**; do
     echo "Formatting $file..."
-    latexindent -s -l "$ROOT/.latexindent.yaml" -c "$OUT_DIR" -w $file
+    "$LATEXINDENT" -s -l "$ROOT/.latexindent.yaml" -c "$OUT_DIR" -w $file
 done
